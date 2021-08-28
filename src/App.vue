001247fcc9ac3b23cb1cdb035b2b38b3187afc4d
hu <348968803@qq.com>
2021-08-28 11:36:29 +0800

    底部高亮效果完成

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    //头部
+<MyHeader title="购物车"></MyHeader>
+<MyTabBar :arr="tabList"></MyTabBar>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import MyTabBar from './components/MyTabBar.vue'
+export default {
+  components: {
+    MyHeader,
+    MyTabBar
+  },
+  data () {
+    return {
+      tabList: [
+        {
+          iconText: 'icon-shangpinliebiao',
+          text: '商品列表',
+          componentName: 'MyGoodsList'
+        },
+        {
+          iconText: 'icon-sousuo',
+          text: '商品搜索',
+          componentName: 'MyGoodsSearch'
+        },
+        {
+          iconText: 'icon-user',
+          text: '我的信息',
+          componentName: 'MyUserInfo'
+        }
+      ]
+    }
+  },
+
+  created () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+  class="my-header"
+  :style="{backgroundColor, color}">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // key 要接受的数据
+    // 值 又是一个约束对象
+    title: {
+      // 约束的数据类型
+      type: String,
+      // 该数据为必传项
+      required: true
+    },
+    backgroundColor: {
+      type: String,
+      // 设置默认值
+      default: '#1d7bff'
+    },
+    color: {
+      type: String,
+      default: '#fff'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .my-header {
+    height: 45px;
+    line-height: 45px;
+    text-align: center;
+    background-color: #1d7bff;
+    color: #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 2;
+  }
+</style>
